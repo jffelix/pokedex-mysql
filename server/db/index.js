f56d091e282f22d4a943e6f53564e72d8a738f0b
Jean-Luc Felix <jean-luc@Jean-Lucs-MacBook-Pro.local>
2021-05-29 22:20:18 -0700

    Rendered App component to the DOM

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,22 @@
 // TODO: Establish connection to mysql database
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+
+  host: 'localhost',
+  user: 'root',
+  database: 'pokedex',
+  password: 'password'
+
+})
+
+db.connect((err, results) => {
+  if (err) {
+    console.log('Error received at db: ', err);
+    return;
+  } else {
+    console.log('Successfully connected at db!');
+  }
+})
+
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,1 +1,12 @@
 // TODO: Create an express server
+
+const express = require('express');
+
+const app = express();
+
+const port = 8080;
+
+// app.list(port, () => {
+//   console.log(`Listening on port: ${port}!`);
+// })
+
